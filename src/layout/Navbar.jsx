a9cfbedec1d86c75ec7ba2e80205df132e01d585
Fatin Israq Talha <israqq2120@gmail.com>
2026-01-01 21:24:26 +0600

    Add mobile menu functionality and toggle button to Navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,6 @@
+import { Menu, X } from "lucide-react";
 import Button from "../components/Button";
-import { Menu } from "lucide-react";
+import { useState } from "react";
 
 const navLinks = [
   { href: "#about", label: "About" },
@@ -9,6 +10,7 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   return (
     <header className="fixed top-0 right-0 left-0 bg-transparent py-5">
       <nav className="container mx-auto px-6 flex items-center justify-between">
@@ -40,10 +42,32 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Menu Button  */}
-        <button className="md:hidden p-2 text-foreground">
-          <Menu size={24} />
+        <button
+          className="md:hidden p-2 text-foreground cursor-pointer"
+          onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+        >
+          {isMobileMenuOpen ? <X size={24}/> : <Menu size={24}></Menu>}
         </button>
       </nav>
+
+      {/* Mobile Menu  */}
+      {isMobileMenuOpen && (
+        <div className="md:hidden glass-strong animation-fade-in">
+          <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+            {navLinks.map((link, index) => (
+              <a
+                href={link.href}
+                key={index}
+                className="text-lg text-muted-foreground hover:text-foreground py-2"
+              >
+                {link.label}
+              </a>
+            ))}
+
+            <Button>Contact Me</Button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
